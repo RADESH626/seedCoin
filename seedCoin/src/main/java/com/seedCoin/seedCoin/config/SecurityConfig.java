3d669559db0c3b188bdb266fa023312209e18919
RADESH626 <egalvis2016@gmail.com>
2025-12-30 19:38:02 -0500

    simplificacion de las configuraciones de seguridad de la app

--- seedCoin/src/main/java/com/seedCoin/seedCoin/config/SecurityConfig.java
+++ seedCoin/src/main/java/com/seedCoin/seedCoin/config/SecurityConfig.java
@@ -2,26 +2,14 @@ package com.seedCoin.seedCoin.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll());
-        return http.build();
-    }
-
-    @Bean
     public PasswordEncoder passwordEncoder() {
-        return new org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder();
     }
 }
