36b60dba20ffb8b2bab5b5bf5b0b1eaa9fa9be17
RADESH626 <egalvis2016@gmail.com>
2025-12-31 14:26:08 -0500

    refactor:se cambio como estan implementadas las categorias en el proyecto

---
+++ seedCoin/src/main/java/com/seedCoin/seedCoin/model/enums/TransactionCategoryExpense.java
@@ -0,0 +1,15 @@
+package com.seedCoin.seedCoin.model.enums;
+
+public enum TransactionCategoryExpense {
+    FOOD,
+    HOUSING,
+    TRANSPORTATION,
+    ENTERTAINMENT,
+    HEALTHCARE,
+    EDUCATION,
+    PERSONAL_CARE,
+    SHOPPING,
+    UTILITIES,
+    FINANCIAL,
+    OTHER_EXPENSE
+}
---
+++ seedCoin/src/main/java/com/seedCoin/seedCoin/model/enums/TransactionCategoryIncomes.java
@@ -0,0 +1,9 @@
+package com.seedCoin.seedCoin.model.enums;
+
+public enum TransactionCategoryIncomes {
+    SALARY,
+    BUSSINESS,
+    INVESTMENT_RETURN,
+    GIFT,
+    OTHER_INCOME
+}
