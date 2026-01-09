9004f1715b740d7415a90b6145543cffe1f851de
RADESH626 <egalvis2016@gmail.com>
2026-01-08 20:29:08 -0500

    style: se cambio  la pocicion del signo + del componente addButton para mejorar el diseño visual

--- frontend/src/components/Dashboard/AddButton.tsx
+++ frontend/src/components/Dashboard/AddButton.tsx
@@ -11,8 +11,8 @@ export default function AddButton({ onClick, label }: AddButtonProps) {
             onClick={onClick}
             className="w-full bg-primary text-white py-4 rounded-xl shadow-md hover:shadow-lg hover:bg-primary-hover transition-all flex items-center justify-center gap-2 group"
         >
-            <Plus className="w-6 h-6 group-hover:scale-110 transition-transform" />
             <span className="font-medium text-lg">{label}</span>
+            <Plus className="w-6 h-6 group-hover:scale-110 transition-transform" />
         </button>
     );
 }
